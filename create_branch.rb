84d7dc085c5f8a2f77e46a9a7d17a0eae748bfff
wallerjake <wallerjake@gmail.com>
2014-01-07 15:41:05 -0600

    Add config settings so you can easily modify them

--- create_branch.rb
+++ create_branch.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
+
+# config settings
+branched_from_user = ''
+push_to_user = ''
+
+
 print "Branch text? "
 branch_name = $stdin.gets.chomp
 
@@ -6,7 +12,7 @@ print "Branch from? "
 branch_from = $stdin.gets.chomp
 
 branch_name = branch_name.strip.downcase.tr(" ", "_").gsub("-", "").gsub("&", "").gsub("__", "_").gsub(":", "")
-output_text = "git remote update; git co -b #{branch_name} upstream/#{branch_from}; git submodule update --init; git push [username] #{branch_name}"
+output_text = "git remote update; git co -b #{branch_name} #{branched_from_user}/#{branch_from}; git submodule update --init; git push #{push_to_user} #{branch_name}"
 
 puts "#{output_text}\n"
 system(output_text)
--- create_pull_request.rb
+++ create_pull_request.rb
@@ -1,5 +1,11 @@
 #!/usr/bin/env ruby
 
+# config settings
+branched_from_remote_name = ''
+branched_from_user = ''
+branched_from_repo_name = ''
+push_from_user = ''
+
 print "Title? "
 title = $stdin.gets.chomp.strip
 
@@ -9,10 +15,10 @@ body = $stdin.gets.chomp.strip
 branch_name = `git rev-parse --abbrev-ref HEAD`.strip
 print "Branch Name: #{branch_name}\n"
 
-branched_from = `git rev-parse --abbrev-ref --symbolic-full-name @{u}`.gsub("[remote_name]/", "").strip
+branched_from = `git rev-parse --abbrev-ref --symbolic-full-name @{u}`.gsub("#{branched_from_remote_name}/", "").strip
 print "Branched From: #{branched_from}\n"
 
-command_to_be_ran = "curl -H 'Content-Type: application/json' -u [username] -H 'Accept: application/json' -X POST -d '{\"title\":\"#{title}\",\"body\":\"#{body}\",\"head\":\"[username]:#{branch_name}\",\"base\":\"#{branched_from}\"}' https://api.github.com/repos/[to_username]/[to_repo]/pulls"
+command_to_be_ran = "curl -H 'Content-Type: application/json' -u #{push_from_user} -H 'Accept: application/json' -X POST -d '{\"title\":\"#{title}\",\"body\":\"#{body}\",\"head\":\"#{push_from_user}:#{branch_name}\",\"base\":\"#{branched_from}\"}' https://api.github.com/repos/#{branched_from_user}/#{branched_from_repo_name}/pulls"
 
 print "\nCommand: #{command_to_be_ran}\n\n"
 
