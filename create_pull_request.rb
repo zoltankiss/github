144669538959bc7626f228c41450b9711f7e269c
wallerjake <wallerjake@gmail.com>
2014-01-08 07:56:03 -0600

    Clean up the branched_from_remote_name as I can just use split instead

--- create_pull_request.rb
+++ create_pull_request.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 
 # config settings
-branched_from_remote_name = ''
 branched_from_user = ''
 branched_from_repo_name = ''
 push_from_user = ''
@@ -15,7 +14,7 @@ body = $stdin.gets.chomp.strip
 branch_name = `git rev-parse --abbrev-ref HEAD`.strip
 print "Branch Name: #{branch_name}\n"
 
-branched_from = `git rev-parse --abbrev-ref --symbolic-full-name @{u}`.gsub("#{branched_from_remote_name}/", "").strip
+branched_from = `git rev-parse --abbrev-ref --symbolic-full-name @{u}`.split('/')[-1].strip
 print "Branched From: #{branched_from}\n"
 
 command_to_be_ran = "curl -H 'Content-Type: application/json' -u #{push_from_user} -H 'Accept: application/json' -X POST -d '{\"title\":\"#{title}\",\"body\":\"#{body}\",\"head\":\"#{push_from_user}:#{branch_name}\",\"base\":\"#{branched_from}\"}' https://api.github.com/repos/#{branched_from_user}/#{branched_from_repo_name}/pulls"
